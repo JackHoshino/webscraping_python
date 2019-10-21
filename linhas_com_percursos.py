5faaeb206e84f60d1f591f50c40bb38938a2aa39
Dagoberto <dagobertomedeiros@gmail.com>
2019-10-21 20:18:41 -0300

    melhorado: coleta de codigos e nomes das linhas, alem de incluir coleta de todo o percurso

--- linhas_com_percursos.py
+++ linhas_com_percursos.py
@@ -4,42 +4,47 @@ import pandas as pd
 import re
 from unicodedata import normalize
 
+
 #removendo acentuação
 def remover_acentos(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
 
+#contando o nº de divs na pagina
 def countDivClass(html, className):
     soup = BeautifulSoup(html.text, 'html.parser')
     count = soup.find_all('div', class_ = className)
     return len(count)
-    
+
+
 link = 'https://www.meubuzu.com.br/linhas?page='
 i=1
-totalPag = 2
+totalPag = 25
 linhasPorPag = 20
 linhasNome = []
+linhasCod = []
 percursoNome = []
 classNameIn = 'col-xs-12 line-item-name'
 
 while(i < totalPag):
     pageOut = requests.get(link + str(i))
-    k = 0
+    k = 1
     if(pageOut.status_code == 200):
         soupOut = BeautifulSoup(pageOut.text, 'html.parser')
         while(k < linhasPorPag):
-            temp = soup.find_all('span', class_ = 'visible-xs')[k].get_text()
-            linhasNome.append(remover_acentos(temp))
-            print(linhasNome)
-            strLinkIn = str(temp.find('a')['href'])
-            print(strLinkIn)
+            tempCodOut = soupOut.find_all('div', class_ = 'col-lg-2 col-sm-2 col-xs-3 text-center hidden-xs')[k].get_text()
+            tempNomeOut = soupOut.find_all('span', class_ = 'hidden-xs')[k].get_text()
+            tempIn = soupOut.find_all('div', class_ = 'col-sm-5 col-xs-10')[k]
+            linhasCod.append(tempCodOut.replace('\n', ''))
+            linhasNome.append(remover_acentos(tempNomeOut))
+            strLinkIn = str(tempIn.find('a')['href'])
             pageIn = requests.get(strLinkIn)
             if(pageIn.status_code == 200):
                 soupIn = BeautifulSoup(pageIn.text, 'html.parser')
                 j = countDivClass(pageIn, classNameIn)
                 m = 0
                 while(m < j):
-                    tempIn = soupIn.find_all(class_ = 'col-xs-12 line-item-name')[m].get_text()
-                    percursoNome.append([remover_acentos(temp), remover_acentos(tempIn)])
+                    tempIn = soupIn.find_all(class_ = classNameIn)[m].get_text()
+                    percursoNome.append(tempCodOut.replace('\n', '') + '>' + remover_acentos(tempIn.replace('\n', '')))
                     m+=1
             k+=1
             if(i == totalPag):
@@ -47,44 +52,7 @@ while(i < totalPag):
                 break
     i+=1
 
+dfLinhasSite = pd.DataFrame(linhasCod, linhasNome)
 
 dfLinhasSite.to_excel('linhasSite.xls')
-dfData = pd.read_excel('/home/dagoberto/Documentos/TRABALHO/regioes.xlsx', 'Plan1')
-df = pd.DataFrame(dfData)
-#dfData['Bairro'].head()
 
-#pegando os codigos linhas
-linhas = []
-bairros = []
-hifen = []
-barra = []
-i = 0
-while(i < len(forecast_itens)):
-    forecast_itens[i] = forecast_itens[i].replace(')', '-')
-    forecast_itens[i] = forecast_itens[i].replace('(', '')
-    forecast_itens[i] = forecast_itens[i].upper()
-    #print(forecast_itens[i].find('-00'))
-    if(forecast_itens[i].find('-00') == 4):
-        linhas.append(forecast_itens[i][0:4])
-    else:
-        linhas.append(forecast_itens[i][0:7])
-        hifen.append(forecast_itens[i].split('-'))
-    i+=1
-
-
-#contando a quantidade de linhas por bairro
-i=0
-#dfData['qtdLinhas'] = 0
-listQtdLinhas = []
-while(i < 169):
-    j=0
-    listQtdLinhas.append(j)
-    while(j < len(forecast_itens)):
-        if(re.search(dfData.loc[i, 'Bairro'], forecast_itens[j]) != None):
-            listQtdLinhas[i] = listQtdLinhas[i]+1
-        j+=1
-    i+=1
-
-dfData['qtdLinhas'] = listQtdLinhas
-
-dfData.to_excel('df_linhas.xls')
